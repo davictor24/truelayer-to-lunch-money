f4305bbf8cc3d79f6a229476d0e5922e12c92710
Victor Durojaiye <durojaiyevic@gmail.com>
2023-12-01 14:26:43 +0000

    Set up frontend and backend projects

---
+++ backend/lunch-money-service/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 8081;
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
